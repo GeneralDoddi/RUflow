b909be48efb4019c11cf52db47c0bb2772349bee
GeneralDoddi <doddisti@gmail.com>
2014-09-23 23:25:59 +0000

    merger

---
+++ src/is/ru/app/DbHelper.java
@@ -0,0 +1,44 @@
+package is.ru.app;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Þórður on 23.9.2014.
+ */
+public class DbHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "FLOW_DB";
+    public static final int DB_VERSION = 1;
+
+    public static final String TableFlow = "flow";
+    public static final String[] TableFlowCols = { "_id", "boardId", "type", "bestTime", "finished" };
+
+    private static final String sqlCreateTableFlow =
+            "CREATE TABLE flow(" +
+                    " _id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " boardId INTEGER NOT NULL," +
+                    " type TEXT NOT NULL" +
+                    " bestTime DATETIME," +
+                    " finished BOOLEAN DEFAULT FALSE " +
+                    ");";
+
+    private static final String sqlDropTableFlow =
+            "DROP TABLE IF EXISTS flow;";
+
+    public DbHelper( Context context ) {
+        super( context, DB_NAME, null, DB_VERSION );
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL( sqlCreateTableFlow );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL( sqlDropTableFlow );
+        onCreate( db );
+    }
+}
---
+++ src/is/ru/app/FlowDbAdapter.java
@@ -0,0 +1,88 @@
+package is.ru.app;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by Þórður on 23.9.2014.
+ */
+public class FlowDbAdapter {
+
+    SQLiteDatabase db;
+    DbHelper dbHelper;
+    Context context;
+
+
+    public FlowDbAdapter( Context c ) {
+        context = c;
+    }
+
+    public FlowDbAdapter openToRead() {
+        dbHelper = new DbHelper( context );
+        db = dbHelper.getReadableDatabase();
+        return this;
+    }
+
+    public FlowDbAdapter openToWrite() {
+        dbHelper = new DbHelper( context );
+        db = dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        db.close();
+    }
+
+    public long insertFlow( int boardId, Timestamp bestTime, String type, boolean finished ) {
+        String[] cols = DbHelper.TableFlowCols;
+        ContentValues contentValues = new ContentValues();
+        contentValues.put( cols[1], ((Integer)boardId).toString() );
+        contentValues.put( cols[2], type);
+        contentValues.put( cols[3], String.valueOf(bestTime));
+        contentValues.put( cols[4], finished);
+        openToWrite();
+        long value = db.insert(DbHelper.TableFlow, null, contentValues );
+        close();
+        return value;
+    }
+
+    public long updateFlow( int boardId, Timestamp bestTime, String type, boolean finished ) {
+        String[] cols = DbHelper.TableFlowCols;
+        ContentValues contentValues = new ContentValues();
+        contentValues.put( cols[1], ((Integer)boardId).toString() );
+        contentValues.put( cols[2], type);
+        contentValues.put( cols[3], String.valueOf(bestTime));
+        contentValues.put( cols[4], finished);
+        openToWrite();
+        long value = db.update(DbHelper.TableFlow,
+                contentValues,
+                cols[1] + "=" + boardId, null );
+        close();
+        return value;
+    }
+
+    public long updateFlowFinished( long id, boolean finished ) {
+        String[] cols = DbHelper.TableFlowCols;
+        ContentValues contentValues = new ContentValues();
+        contentValues.put( cols[4], finished);
+        openToWrite();
+        long value = db.update(DbHelper.TableFlow,
+                contentValues,
+                cols[0] + "=" + id, null );
+        close();
+        return value;
+    }
+
+
+    public Cursor queryFlows(int id, String type) {
+        openToRead();
+        Cursor cursor = db.query( DbHelper.TableFlow,
+                DbHelper.TableFlowCols, "type =" + type + " AND boardId =" + id, null  , null, null, null);
+        return cursor;
+    }
+
+}
